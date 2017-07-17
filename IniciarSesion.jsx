4fd59934bb3f5369abf7c8d1f30746b69543365a
Gerlin Orlando Torres Saavedra <gerlinorlandotorressaavedra@192.168.0.27>
2017-07-16 19:42:00 -0500

    se obtiene los usuarios del servicio

--- IniciarSesion.jsx
+++ IniciarSesion.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as request from 'superagent';
 
 class IniciarSesion extends React.Component {
     constructor() {
@@ -30,21 +31,69 @@ class IniciarSesion extends React.Component {
                             <span className={this.state.mostrarErrorContrasena}>Error: campo requerido</span>
                         </div>
                         <span className={this.state.mostrarErrorForm}>{this.state.msnErrorForm}</span>
-                        <button className="btn btn-success" type="button" onClick = {this.validarUsuario}>Ingresar</button>
+                        <button className="btn btn-success" type="button" onClick = {this.validarUsuario.bind(this)}>Ingresar</button>
                     </form>
                 </div>
             </div>
         );
     }
     /**
+     * Oculta los mensajes de error.
+     */
+    ocultarMsnError(){
+        this.setState({
+            mostrarErrorCorreo: 'ocultar-elemento',
+            mostrarErrorContrasena: 'ocultar-elemento',
+            mostrarErrorForm: 'ocultar-elemento',
+            msnErrorForm: 'Error desconocido'
+        });
+    }
+    /**
      * Valida el usuario del formulario.
      */
     validarUsuario(){
-       let formularioLogin = document.getElementById("form-login");
-       console.log(formularioLogin.checkValidity());
-       let inputCorreo = formularioLogin['correo-input'];
-       console.log(inputCorreo.checkValidity());
-       console.log(inputCorreo);
+        this.ocultarMsnError();
+        let formularioLogin = document.getElementById("form-login");
+        let inputCorreo = formularioLogin['correo-input'];
+        if(!inputCorreo.checkValidity()){
+            this.setState({
+                mostrarErrorCorreo: 'mostrar-elemento-block'
+            });
+        }
+        let inputContrasena = formularioLogin['contrasena-input'];
+        if(!inputContrasena.checkValidity()){
+            this.setState({
+                mostrarErrorContrasena: 'mostrar-elemento-block'
+            });
+        }
+        if(inputContrasena.checkValidity() && inputCorreo.checkValidity()){
+            let correo = inputCorreo.value;
+            let pass = inputContrasena.value;
+            request
+            .get('https://tienda-9303e.firebaseio.com/usuarios.json')
+            .set('Content-Type', 'application/json')
+            .end((err, res)=>{
+                console.log(res.body);
+                let usuarios = res.body;
+                let usuario = undefined;
+                for (var index = 0; index < usuarios.length; index++) {
+                    var element = usuarios[index];
+                    if(element.correo === correo && element.contrasena == pass){
+                        usuario = element;
+                        break;
+                    }
+                }
+                if(undefined == usuario){
+                    this.setState({
+                        mostrarErrorForm: 'mostrar-elemento-block',
+                        msnErrorForm: 'Usuario no encontrado'
+                    });
+                }else{
+                    //TODO implementar la redireccion.
+                }
+            })
+            
+        }
     }
 }
 
